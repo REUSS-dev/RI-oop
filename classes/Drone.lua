576d5f129303a31b449a7de3c41588928dd600ab
REUSS <mreu@dr.com>
2024-06-26 23:42:12 +0700

    initial commit

---
+++ classes/Drone.lua
@@ -0,0 +1,38 @@
+-- Drone
+local Drone = {}
+
+
+
+-- documentation
+
+
+
+-- config
+
+
+
+-- consts
+
+
+
+-- vars
+
+
+
+-- init
+
+
+
+-- fnc
+
+
+
+-- classes
+
+
+
+-- Drone fnc
+
+
+
+return Drone
