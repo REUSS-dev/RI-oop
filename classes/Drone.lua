cb4d21adb89d4e013db9f97611f041c82fe46c50
REUSS <mreu@dr.com>
2024-06-27 02:57:28 +0700

    Persist resource state and send it to circular body

--- classes/Drone.lua
+++ classes/Drone.lua
@@ -63,13 +63,21 @@ end
 ---@param velocity number?
 ---@param resource boolean?
 function Drone.new(x, y, angle, velocity, resource)
+    local resourceValue
+    if resource == nil then
+        resourceValue = math.random(0, 1) == 0
+    end
+    
     ---@class Drone
     local obj = {
-        res = resource
+        res = resourceValue
     }
 
-    obj.body = body.new(DRONE_BODY_TYPE, DRONE_RADIUS, DRONE_COLOR)
-    obj.controller = controller.new(x, y, obj.body, velocity or math.random(DRONE_VELOCITY_MINIMUM, DRONE_VELOCITY_MAXIMUM), angle or math.rad(math.random(1, 360)))
+    
+    obj.body = body.new(DRONE_BODY_TYPE, DRONE_RADIUS, DRONE_COLOR) --[[@as BodyCircular]]
+    obj.controller = controller.new(x, y, obj.body--[[@as Body]], velocity or math.random(DRONE_VELOCITY_MINIMUM, DRONE_VELOCITY_MAXIMUM), angle or math.rad(math.random(1, 360)))
+    
+    obj.body:setFill(obj.res)
 
     setmetatable(obj, Drone_meta)
 
