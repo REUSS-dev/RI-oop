f78ec50ffdd551c71b6368eface4b0d59ce36662
REUSS <mreu@dr.com>
2024-06-29 03:50:49 +0700

    World class: Resource and base implementations

--- classes/World.lua
+++ classes/World.lua
@@ -1,11 +1,12 @@
 -- World
 local World = {}
 
+local dest = require("classes.Destination")
 local drone = require("classes.Drone")
 
 -- documentation
 
-
+---@alias ObjectID number
 
 -- config
 
@@ -13,15 +14,22 @@ local drone = require("classes.Drone")
 
 -- consts
 
-
+---@enum WorldObjectType
+World.WorldObjectType = {
+    BASE = 1,
+    RESOURCE = 2
+}
 
 -- vars
 
-
+calls = 0
 
 -- init
 
+dest.registerType(World.WorldObjectType.BASE, {0, 1, 1})
+dest.registerType(World.WorldObjectType.RESOURCE, {1, 1, 0})
 
+drone.setCountersCount(2)
 
 -- fnc
 
@@ -31,8 +39,27 @@ local drone = require("classes.Drone")
 
 ---@class World
 ---@field drones Drone[]
+---@field worldObjects Destination[]
 local world = {}
 local World_meta = {__index = world}
+
+function world:placeBase(x, y)
+    local base = dest.new(x, y, World.WorldObjectType.BASE)
+    table.insert(self.worldObjects, base)
+
+    return #self.worldObjects
+end
+
+function world:placeResource(x, y)
+    local res = dest.new(x, y, World.WorldObjectType.RESOURCE)
+    table.insert(self.worldObjects, res)
+
+    return #self.worldObjects
+end
+
+function world:removeObject(id)
+    table.remove(self.worldObjects, id)
+end
 
 function world:repopulate()
     self.drones = {}
@@ -48,7 +75,7 @@ end
 function world:tick(dt)
     for i = 1, self.dronesCount do
         local currentDrone = self.drones[i]
-        local x, y, angle = currentDrone:getDroneDirectionals()
+        local x, y, angle = currentDrone:getDirectionals()
         
         if x < 0 then
             if math.cos(angle) < 0 then
@@ -70,6 +97,19 @@ function world:tick(dt)
             end
         end
 
+        for _, worldObject in ipairs(self.worldObjects) do
+            if worldObject.controller:checkCollision(currentDrone.controller) then
+                local objectType = worldObject:getType()
+
+                currentDrone:resetCounter(objectType)
+
+                if (objectType == World.WorldObjectType.BASE and not currentDrone:hasResource()) or (objectType == World.WorldObjectType.RESOURCE and currentDrone:hasResource()) then
+                    currentDrone:toggleResource()
+                    currentDrone.controller:reverseAngle()
+                end
+            end
+        end
+
         currentDrone:tick(dt)
     end
 end
@@ -77,9 +117,16 @@ end
 function world:paint()
     love.graphics.rectangle("line", 0, 0, self.width, self.height)
 
+    for _, object in ipairs(self.worldObjects) do
+        object:draw()
+    end
+
     for _, droneToDraw in ipairs(self.drones) do
         droneToDraw:draw()
     end
+
+    love.graphics.print(tostring(calls))
+    calls = 0
 end
 
 -- World fnc
@@ -89,20 +136,33 @@ end
 ---@param height number
 ---@param droneAmount number
 ---@return World
-function World.new(width, height, droneAmount)
+function World.new(width, height, droneAmount, placePoints)
 
     ---@class World
     local obj = {
         width = width,
         height = height,
         dronesCount = droneAmount,
-        drones = {}
+        drones = {},
+        worldObjects = {}
     }
 
     setmetatable(obj, World_meta)
 
     obj:repopulate()
-    
+
+    if placePoints then
+        obj:placeBase(
+            math.random(dest.getDestinationPointRadius(), obj.width - dest.getDestinationPointRadius()),
+            math.random(dest.getDestinationPointRadius(), obj.height - dest.getDestinationPointRadius())
+        )
+
+        obj:placeResource(
+            math.random(dest.getDestinationPointRadius(), obj.width - dest.getDestinationPointRadius()),
+            math.random(dest.getDestinationPointRadius(), obj.height - dest.getDestinationPointRadius())
+        )
+    end
+
     return obj
 end
 
